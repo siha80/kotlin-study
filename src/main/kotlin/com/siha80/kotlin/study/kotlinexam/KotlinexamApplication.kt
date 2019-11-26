66c33d378cebf7735e044f094af0f07a98861da4
Sungil.Ha <siha80@linecorp.com>
2019-11-26 10:16:11 +0900

    setup kotlin project with spring boot

---
+++ src/main/kotlin/com/siha80/kotlin/study/kotlinexam/KotlinexamApplication.kt
@@ -0,0 +1,11 @@
+package com.siha80.kotlin.study.kotlinexam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinexamApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinexamApplication>(*args)
+}
---
+++ src/test/kotlin/com/siha80/kotlin/study/kotlinexam/KotlinexamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.siha80.kotlin.study.kotlinexam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinexamApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
